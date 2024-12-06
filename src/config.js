7e422f798d3e8d016973a6278cfa967894e9b574
am-MongoDB <andrew.morgan@mongodb.com>
2024-12-06 15:47:20 +0000

    Update location for maps and weather to Maidenhead

--- src/config.js
+++ src/config.js
@@ -1,9 +1,8 @@
 export let config = {
     formKey: '566e2acc-e586-41b4-afc6-e1d4a4f4b743',
     weatherKey: 'ddc83cec21a743aab4a91a091b83746f',
-    // openWeatherKey: 'c569fc8cf2102b74d75c2439b9079c07',
     openWeatherKey: 'f36a2ddc5e33d14d3708b92f20f1d315',
-    lat: '45.8708907',
-    lon: '-1.2491633',
-    location: 'Oléron Villa'
+    lat: '51.5206777',
+    lon: '-0.7401434',
+    location: 'Thieves Garden'
 }
