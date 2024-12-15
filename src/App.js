a62f0824a6a6c0654afc6dbbf0b4be37fdae6e3b
am-MongoDB <andrew.morgan@mongodb.com>
2024-12-15 12:20:39 +0000

    Remove some of the French localisation

--- src/App.js
+++ src/App.js
@@ -4,19 +4,12 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Layout from './pages/Layout';
 import Home from './pages/Home';
-import HomeFR from './pages/HomeFR';
 import InsideHouse from './pages/InsideHouse';
-import InsideHouseFR from './pages/InsideHouseFR';
 import OutsideHouse from './pages/OutsideHouse';
-import OutsideHouseFR from './pages/OutsideHouseFR';
 import Bookings from './pages/Bookings';
-import BookingsFR from './pages/BookingsFR';
 import Contact from './pages/Contact';
-import ContactFR from './pages/ContactFR';
 import Facilities from './pages/Facilities';
-import FacilitiesFR from './pages/FacilitiesFR';
 import Location from './pages/Location';
-import LocationFR from './pages/LocationFR';
 
 export const UserContext = createContext(null);
 
@@ -33,19 +26,12 @@ function App() {
           <Routes>
             <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
-            <Route path="fr" element={<HomeFR />} />
             <Route path="bookings" element={<Bookings />} />
-            <Route path="bookings-fr" element={<BookingsFR />} />
             <Route path="contact" element={<Contact />} />
-            <Route path="contact-fr" element={<ContactFR />} />
             <Route path="inside_house" element={<InsideHouse />} />
-            <Route path="inside_house-fr" element={<InsideHouseFR />} />
             <Route path="outside_house" element={<OutsideHouse />} />
-            <Route path="outside_house-fr" element={<OutsideHouseFR />} />
             <Route path="facilities" element={<Facilities />} />
-            <Route path="facilities-fr" element={<FacilitiesFR />} />
             <Route path="location" element={<Location />} />
-            <Route path="location-fr" element={<LocationFR />} />
             <Route path="*" element={<Home />} />
             </Route>
           </Routes>
