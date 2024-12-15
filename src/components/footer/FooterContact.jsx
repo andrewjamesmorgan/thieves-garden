70a74a115a4d9b88d0e89fd5f75f08f7896448ef
am-MongoDB <andrew.morgan@mongodb.com>
2024-12-15 12:55:50 +0000

    Change to my phone number in footer

--- src/components/footer/FooterContact.jsx
+++ src/components/footer/FooterContact.jsx
@@ -1,13 +1,8 @@
-// import { useContext } from 'react';
-// import { UserContext } from '../../App';
 import AwesomeButton from '../buttons/AwesomeButton';
 
 export default function FooterContact() {
-  // const { language } = useContext(UserContext);
-
   return (
     <div className="footer-column" id="footer-contact">
-      {/* <h5>{language === "fr" ? "Contactez-nous" : "Contact us"}</h5> */}
       <div className="stacked-buttons">
         <AwesomeButton 
           label="email" 
@@ -16,11 +11,11 @@ export default function FooterContact() {
         <AwesomeButton 
           label="WhatsApp" 
           icon="fa fa-whatsapp" 
-          url="https://wa.me/447899984259"/>
+          url="https://wa.me/447833483595"/>
         <AwesomeButton 
-          label="+44-7899984259" 
+          label="+44-7833483595" 
           icon="fa-phone" 
-          url="tel:+447899984259"/>
+          url="tel:+447833483595"/>
       </div>
     </div>
   );
