2584e37523c91a8a3bf73cf68087afd346eeb0ab
am-MongoDB <andrew.morgan@mongodb.com>
2025-01-12 11:28:28 +0000

    Reorder booking  data so that the latest bookings are at the top

--- src/data/bookingData.jsx
+++ src/data/bookingData.jsx
@@ -1,45 +1,45 @@
 export const bookingData = [
-    {
-      property: 'Thieves Garden',
-      startDate: new Date('2024-12-17'),
-      endDate: new Date('2024-12-20'),
-      status: 'booked',
-      booking: {
-        name: 'Colin Mackay',
-        email: 'colinjohnmackay49@gmail.com',
-        source: 'direct'
-      },
-      comments: 'Visiting family. Repeat renter'
+  {
+    property: 'Thieves Garden',
+    startDate: new Date('2025-06-08'),
+    endDate: new Date('2025-06-20'),
+    status: 'booked',
+    booking: {
+      name: 'Holly Williams',
+      source: 'Airbnb'
     },
-    {
-      property: 'Thieves Garden',
-      startDate: new Date('2024-12-20'),
-      endDate: new Date('2024-12-24'),
-      status: 'tentative',
-      comments: 'Need time to clean'
+    comments: 'Will need sheets for the sofa bed for the middle weekend.'
+  },
+  {
+    property: 'Thieves Garden',
+    startDate: new Date('2025-01-05'),
+    endDate: new Date('2025-01-12'),
+    notes: "Had originally booked for 3 weeks, but only stayed/paid for 1",
+    status: 'booked',
+    booking: {
+      name: 'Simon Nagy',
+      email: 'simon.nagy@hotmail.com',
+      source: 'direct'
     },
-    {
-      property: 'Thieves Garden',
-      startDate: new Date('2025-01-05'),
-      endDate: new Date('2025-01-12'),
-      notes: "Had originally booked for 3 weeks, but only stayed/paid for 1",
-      status: 'booked',
-      booking: {
-        name: 'Simon Nagy',
-        email: 'simon.nagy@hotmail.com',
-        source: 'direct'
-      },
-      comments: 'Coming from Australia to work as a teacher'
+    comments: 'Coming from Australia to work as a teacher'
+  },
+  {
+    property: 'Thieves Garden',
+    startDate: new Date('2024-12-17'),
+    endDate: new Date('2024-12-20'),
+    status: 'booked',
+    booking: {
+      name: 'Colin Mackay',
+      email: 'colinjohnmackay49@gmail.com',
+      source: 'direct'
     },
-    {
-      property: 'Thieves Garden',
-      startDate: new Date('2025-06-08'),
-      endDate: new Date('2025-06-20'),
-      status: 'booked',
-      booking: {
-        name: 'Holly Williams',
-        source: 'Airbnb'
-      },
-      comments: 'Will need sheets for the sofa bed for the middle weekend.'
-    }
-  ];
+    comments: 'Visiting family. Repeat renter'
+  },
+  {
+    property: 'Thieves Garden',
+    startDate: new Date('2024-12-20'),
+    endDate: new Date('2024-12-24'),
+    status: 'tentative',
+    comments: 'Need time to clean'
+  }
+];
