328c17436dd282d6f0959051e195c911c6fbce97
am-MongoDB <andrew.morgan@mongodb.com>
2025-02-04 14:39:51 +0000

    Prep for fetching booking data

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
-export let config = {
+export const config = {
     formKey: '566e2acc-e586-41b4-afc6-e1d4a4f4b743',
     weatherKey: 'ddc83cec21a743aab4a91a091b83746f',
     openWeatherKey: 'f36a2ddc5e33d14d3708b92f20f1d315',
+    getTGBookingURL: 'https://buic2mp6aid76yuv7a5bjlodxy0ptunc.lambda-url.eu-west-1.on.aws?secret=b2F8JvLxM7qPzYhgfdhj779KrNsQ3CwA4',
     lat: 51.5206777,
     lon: -0.7401434,
     mapId: '15019cba5d15b2ed',
--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -1,11 +1,7 @@
-// import { useContext } from 'react';
-// import { UserContext } from '../App';
 import LocationHead from '../components/location/LocationHead';
 import LocationContent from '../components/location/LocationContent';
 
 export default function Location() {
-  // const { language } = useContext(UserContext);
-  
   return (
     <>
       <LocationHead />
